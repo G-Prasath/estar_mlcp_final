2501e14f700e299849888d7a41d7d0efcf02abb8
G-Prasath <guruprasathmsc@gmail.com>
2024-08-10 16:41:27 +0530

    Add Banner Images for webp

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -4,14 +4,20 @@ const Banner = () => {
   const bannerCnt = [
     {
       img: "/banner/Shuttle-Parking.avif",
+      imgWebp: "/banner/Shuttle-Parking.webp",
+      imgSizes: "(max-width: 1024px) 100vw, (max-width: 640px) 90vw, 80vw",
       text: "Streamlined Shuttle Parking",
     },
     {
       img: "/banner/Tower-Parking.avif",
+      imgWebp: "/banner/Tower-Parking.webp",
+      imgSizes: "(max-width: 1024px) 100vw, (max-width: 640px) 90vw, 80vw",
       text: "Innovative Tower Parking Solutions",
     },
     {
       img: "/banner/amrisar-project.avif",
+      imgWebp: "/banner/amrisar-project.webp",
+      imgSizes: "(max-width: 1024px) 100vw, (max-width: 640px) 90vw, 80vw",
       text: "Inhouse Engineered Patented Products",
     },
   ];
@@ -30,22 +36,24 @@ const Banner = () => {
       {bannerCnt.map((cnt, index) => (
         <div key={index}>
           <div className="absolute inset-0">
-            <img
-              src={cnt.img}
-              alt={`Background ${index}`}
-              className={`absolute inset-0 object-cover object-center w-full h-full transition-opacity duration-1000 ease-in-out ${
-                index === currentImageIndex ? "opacity-100" : "opacity-0"
-              }`}
-            />
+            <picture>
+              <source srcSet={`${cnt.img} 1024w, ${cnt.img} 640w, ${cnt.img} 320w`} sizes={cnt.imgSizes} type="image/avif" />
+              <source srcSet={`${cnt.imgWebp} 1024w, ${cnt.imgWebp} 640w, ${cnt.imgWebp} 320w`} sizes={cnt.imgSizes} type="image/webp" />
+              <img
+                src={cnt.img}
+                alt={`Background ${index}`}
+                className={`absolute inset-0 object-cover object-center w-full h-full transition-opacity duration-1000 ease-in-out ${
+                  index === currentImageIndex ? "opacity-100" : "opacity-0"
+                }`}
+              />
+            </picture>
             <div className="absolute inset-0 bg-black opacity-40"></div>
           </div>
 
           <div className="relative text-primary z-10 flex flex-col justify-center items-center h-full text-center">
-            <p
-              className={`absolute card-glass text-center mt-[50%] text-5xl font-bold leading-tight mb-4 max-sm:text-lg max-sm:mb-1 transition-opacity duration-1000 ease-in-out ${
+            <p className={`absolute card-glass text-center mt-[50%] text-5xl font-bold leading-tight mb-4 max-sm:text-lg max-sm:mb-1 transition-opacity duration-1000 ease-in-out ${
                 index === currentImageIndex ? "opacity-100" : "opacity-0"
-              }`}
-            >
+              }`}>
               {cnt.text}
             </p>
           </div>
