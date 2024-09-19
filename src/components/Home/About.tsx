dc4c91ae09b75a2ad81a857c14189633e4d4d048
G-Prasath <guruprasathmsc@gmail.com>
2024-09-19 17:04:41 +0530

    Home about changed

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -3,7 +3,6 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 
 const About = () => {
-
   return (
     <div className="sec-padding flex items-center w-full max-lg:flex-col relative">
       <div
@@ -13,7 +12,7 @@ const About = () => {
         className="w-1/2 max-lg:w-full max-lg:order-1 rounded-lg overflow-hidden"
       >
         <LazyLoadImage
-          src="/home/Multi-Level-Car-Parking.webp" 
+          src="/home/Multi-Level-Car-Parking.webp"
           alt="Multi Level Car Parking"
           title="Multi Level Car Parking Estar"
           className="transition-all duration-300 ease-in-out hover:scale-[1.1] w-full h-full"
@@ -27,31 +26,30 @@ const About = () => {
       >
         <h1 className="font-bold text-[25px] text-white text-center">
           Multi Level Car Parking
-        </h1>.
-        <p className="text-white my-5 max-lg:my-2 text-justify">
-          As leaders in the manufacturing of Multi-Level Car Parking (MLCP)
-          systems, we hold Indian and International patents in order to leverage
-          cutting-edge technology and precision engineering. Our comprehensive
-          range includes both dependent and independent systems, reflecting our
-          unwavering dedication to quality and innovation.
+        </h1>
+        .
+        <p className="text-white my-5 max-lg:my-1 text-justify">
+          E Star is a leader in manufacturing multi level car parking systems,
+          offering innovative solutions to tackle space constraints in urban
+          areas. Our advanced automated systems and multi-level structures are
+          designed to optimize parking efficiency across commercial,
+          residential, institutional, and industrial projects.
         </p>
-        <p className="text-white my-5 max-lg:my-2 text-justify">
-          Our parent organization was laid out in 1995, laying the basis for our
-          obligation to greatness. North of twenty years, we have developed to
-          address client issues, encouraging getting through organizations
-          worldwide.
+        <p className="text-white my-5 max-lg:my-1 text-justify">
+          Our Car parking solutions maximize space utilization, reduce land
+          costs, and streamline vehicle flow while providing flood protection
+          for your car. Whether you require a vertical parking system or a
+          multi-level car parking structure, we offer fully customizable designs
+          to meet your exact specifications. Each project is built with durable
+          materials and innovative technology to ensure long-term performance
+          and safety.
         </p>
-
-        <p className="text-white my-5 max-lg:my-2 text-justify">
-          In 2012, we established our very own exclusive car parking division,
-          which emerged to redefine convenience and safety in vehicle parking
-          with patented tech-driven solutions. Through our patented technology,
-          we prioritize accessibility, security, and sustainability, setting new
-          benchmarks in the industry.
+        <p className="text-white my-5 max-lg:my-1 text-justify">
+          By choosing E Star, you invest in eco-friendly, cost-effective
+          solutions that enhance parking capacity without compromising quality.
+          Our modular systems are scalable and adaptable, built to meet your
+          present and future needs.
         </p>
-
-
-
         <Link
           to="/about-us"
           className="text-white font-bold border rounded-full border-white px-5 py-2 mt-3 inline-block hover:text-primary hover:bg-white"
